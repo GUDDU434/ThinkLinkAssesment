4eba1f0c559e28158e744c2bf09ead86c25b3256
GUDDU434 <99042645+GUDDU434@users.noreply.github.com>
2022-09-16 17:15:42 +0530

    nodemailer setup and data saving to the mongodb

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const connection = require("./databaseComponent/dataBase");
 const router = require("./router/router");
+const cors = require("cors");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("welcome to api server");
@@ -12,7 +14,7 @@ app.get("/", (req, res) => {
 
 app.use("/api", router);
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 app.listen(port, async () => {
   await connection;
   console.log("app Started on port 8080");
--- router/router.js
+++ router/router.js
@@ -1,21 +1,75 @@
-const {Router} = require("express")
+const { Router } = require("express");
 const hbs = require("handlebars");
 const nodemailer = require("nodemailer");
-const router = Router()
+const router = Router();
+const axios = require("axios");
+const cryptoPriceModel = require("../modal/model");
+require("dotenv/config");
+
+const HOST = process.env.HOST; //getting the url from the .env file
+const USER = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
 
 const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
+  host: HOST,
   port: 2525,
   auth: {
-    user: "88eb82a6106c36",
-    pass: "4250307e533cf7",
+    user: USER,
+    pass: PASSWORD,
   },
 });
 
-router.get("/prices/btc", async(request,response)=>{
-    const {date, offset, limit} = request.query;
+router.post("/prices/btc", async (request, response) => {
+  const { date, page, limit } = request.query;
+  const { max, min,email } = req.body;
+  
+  console.log(date, offset, limit);
+  let curr_val;
 
-    console.log(date,offset,limit);
-})
+  setInterval(() => {
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/bitcoin/history?date=${date}`
+      )
+      .then(({ data }) => {
+        console.log(data);
+        curr_val = data.market_data.current_price.usd;
+        const price = new cryptoPriceModel({price:curr_val, coin:"btc"})
+        await price.save()
+
+      })
+      .catch((err) => console.log(err));
+  }, 10 * 1000);
+
+  const content = `<div>
+    <h3>Hey there, {{Email}} </h3>
+    <p>The price value of Bitcoin(BTC) in USD goes {{up_down}} by {{diff}}</p>
+    <p>Current value: {{current}}</p>
+  </div>`;
+  const template = hbs.compile(content);
+
+  if (curr_val > max) {
+    transport.sendMail({
+      from: "example.mailtrap.io",
+      to: email,
+      subject: "Bitcoin price update",
+      html: template({ Email: email, up_down: "Up", diff: curr_val - max }),
+    });
+  } else if (curr_val < min) {
+    transport.sendMail({
+      from: "example.mailtrap.io",
+      to: email,
+      subject: "Bitcoin price update",
+      html: template({ Email: email, up_down: "down", diff: min - curr_val }),
+    });
+  }
+
+  if(page==undefined){
+    page=1
+  }
+
+  let data = await cryptoPriceModel.find().skip((page-1)*100).limit(page*100)
+  response.status(200).send(data)
+});
 
 module.exports = router;
